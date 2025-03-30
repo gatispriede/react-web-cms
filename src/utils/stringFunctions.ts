6f6453ef924219d944f5784ced61ece0e1c54a98
Funisimo <gatiss.priede@gmail.com>
2025-03-30 12:40:35 +0300

    Fix

--- src/utils/stringFunctions.ts
+++ src/utils/stringFunctions.ts
@@ -1,4 +1,7 @@
 export const sanitizeKey = (key: string) => {
+    if(typeof key === undefined || typeof key.replace === undefined){
+        return key
+    }
     const specialsRemoved = key.replace(/[~`\s!@#$%^&*\(\)+={}\[\];:\'\"<>.,\/\\-_]/gm, '');
     if (specialsRemoved.length > 30) {
         return specialsRemoved.substr(0, 30);
